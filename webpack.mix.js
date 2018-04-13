28c28cd50f5cd8c56809e615e15e6e82e4b827e7
Dexter Bengil <dexterb2992@gmail.com>
2018-04-13 19:53:02 +0800

    removed versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,4 @@ const { mix } = require('laravel-mix');
  
 mix.js('resources/assets/js/app.js', 'public/js/app.js')
    .js('resources/assets/js/embed.js', 'public/js/embed.js')
-   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
-   .version();
+   .sass('resources/assets/sass/app.scss', 'public/css/app.css');
