cfc4ad0137fd5d64449d544e4177995626894c22
Dexter Bengil <dexterb2992@gmail.com>
2018-07-04 04:33:21 +0800

    fix bug on /slides/create link for upgrade to premium

--- resources/assets/js/views/Slide/Form.vue
+++ resources/assets/js/views/Slide/Form.vue
@@ -36,7 +36,7 @@
                                     <label :for="'_'+type.id">{{ type.value }}</label>
                                 </div>
 
-                                <div v-if="!this.$parent.user.subscribed" style="margin-bottom: 20px;">
+                                <div v-if="user.subscribed == false" style="margin-bottom: 20px;">
                                     <router-link to="/subscribe" class="text-danger" style="font-weight:bold;">
                                         Upgrade to Premium to get access to Opt-in and Social Features
                                     </router-link>
@@ -424,7 +424,8 @@
                 dtpConfig: {
                     format: 'DD/MM/YYYY',
                     useCurrent: false,
-                }
+                },
+                user: {}
             };
         },
 
@@ -449,6 +450,8 @@
                 if (!!user && user.perms) {
                     this.types = user.perms;
                 }
+
+                this.user = user;
             });
 
             Event.listen('slideLayoutChanged', () => {
